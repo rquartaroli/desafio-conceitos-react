2eccc8dbc720508eac0c770814e544f275a9ffe7
Rafael Quartaroli <rafaelquartaroli@hotmail.com>
2021-03-25 18:10:05 -0300

    Challenge finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -13,17 +13,57 @@ interface Task {
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+  
+  let randomLimit = 5;
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    
+    if (tasks.length == randomLimit) {
+      alert('Excedeu o limite de tasks permitidas, se quiser criar mais, exclua uma existente');
+      return;
+    }
+
+    let idRandom = Math.ceil(Math.random() * randomLimit);    
+
+    while (tasks.findIndex(task => task.id === idRandom) >= 0) {
+      idRandom = Math.ceil(Math.random() * randomLimit);
+    }        
+
+    if (newTaskTitle === '') {      
+      return;
+    }
+
+    let newTask: Task = {
+      id: idRandom,
+      title: newTaskTitle,
+      isComplete: false,
+    }    
+
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
+    let updateCompleted = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(updateCompleted);
+    
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const taskIndex = tasks.filter(task => task.id !== id);    
+
+    setTasks(taskIndex);
+    
   }
 
   return (
